fbfa433580f7140459429a0afe242f4b71638420
SabuhiTapdigli <sabuhitapdigli@gmail.com>
2023-12-04 16:07:34 +0400

    new changes

--- lib/getData.ts
+++ lib/getData.ts
@@ -1,17 +1,36 @@
 import { headers } from "next/headers";
 
 const useFetch = async (gclid: any) => {
-  const ip = headers().get("x-forwarded-for");
+  // Fetch the IP address from the headers
+  const ip = headers().get("x-forwarded-for") || "No IP Found";
 
+  // Log the IP address for debugging
+  console.log("IP Address:", ip);
+  console.log("GCLID:", gclid);
+
+  // Define the URL you are sending the request to
   const url = `https://api.gameindustrytitans.com/logic`;
 
-  const res = await fetch(url, {
-    cache: "no-store",
-    method: "POST",
-    body: JSON.stringify({ address: ip, gclid: gclid }),
-  });
-  const data = await res.json();
-  return data;
+  try {
+    // Make the POST request to the server
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        // Optionally include this if your server checks for this header
+        "X-Forwarded-For": ip,
+      },
+      body: JSON.stringify({ ip: ip, gclid: gclid }),
+    });
+
+    // Parse the JSON response
+    const data = await response.json();
+    console.log("Response Data:", data);
+
+    return data;
+  } catch (error) {
+    console.error("Error in useFetch:", error);
+  }
 };
 
 export default useFetch;
